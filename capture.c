9d3ff2a9a3be0117fbfe978dfe93588a0bd97805
Marty Leisner <maleisner@gmail.com>
2019-03-12 20:09:25 -0400

    interim save -- lots of coding -- debugging

--- capture.c
+++ capture.c
@@ -4,15 +4,66 @@
 #include <stdlib.h>
 #include <pcap.h>
 #include <unistd.h>
+#include <limits.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <assert.h>
 
 
 #define SNAP_LEN   1518
+
+
+enum opt_name {
+        opt_endofopt = 0,
+        opt_comment = 1,
+        shb_hardware = 2,
+        shb_os = 3,
+        shb_userappl = 4,
+        if_name = 2,
+        if_description = 3,
+        if_IPv4addr = 4,
+        if_IPv6addr = 5,
+        if_MACaddr = 6,
+        if_EUIaddr = 7,
+        if_speed = 8,
+        if_tsresol = 9,
+        if_tzone = 10,
+        if_filter = 11,
+        if_os = 12, 
+        if_fsclen = 13,
+        if_tsoffset = 14,
+        if_hardware = 15
+};
+
+enum block_type {
+        interface_description = 1,
+        packet_block = 2,
+        simple_packet_block = 3,
+        name_resolution_block = 4,
+        interface_statistics_block = 5,
+        enhanced_packet_block = 6,
+        section_header_block = 0x0a0d0d0a
+};
+
+
+static int output_fd = -1;
+
+struct block {
+	void *data;
+	int size;
+};
+
+static const uint32_t zero = 0;
+static struct block end_of_opt = {
+		.data = &zero,
+		.size = sizeof zero
+};
 
 static void usage(void)
 {
@@ -22,12 +73,152 @@ static void usage(void)
 
 static void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-	fprintf(stderr, "\ntime = %d:%06d, caplen = %d, len = %d\n",
+	fprintf(stderr, "\ntime = %ld:%06ld, caplen = %d, len = %d\n",
 			header->ts.tv_sec, header->ts.tv_usec, header->caplen, header->len);
 }
 
+static int round_to_dword(int n)
+{
+	return (n + 3)   & ~0x3;
+}
 
-main(int argc, char *argv[])
+/* write a section of blocks -- compute how the total we need (each block needs to rounded up to  a double word */
+static bool write_block(enum block_type type,  ...)
+{
+	
+	const int MAX_BLOCK = 2000;
+	va_list ap;
+	int block_size = 0;
+	char block[MAX_BLOCK];
+	char *current;  // where inserting into current block;
+	int result;
+
+	*(uint32_t *) block = type;
+
+	block_size = 8;
+	current = &block[block_size];
+
+	va_start(ap, type);
+	while(1) {
+		struct block *this;
+
+		this = va_arg(ap, struct block *);
+		if(!this) 
+			break;
+
+		memcpy(current, this->data, this->size);
+		current += this->size;
+		block_size += this->size;
+	}
+
+	*(int *) (block + block_size) = block_size + 4;
+	block_size += 4;
+	*(int *) (block + 4) = block_size;
+	result = write(output_fd, block, block_size);
+	assert(result == block_size);
+	return true;	
+}
+
+static void free_ascii_option(struct block *ascii_option)
+{
+	free(ascii_option->data);
+	free(ascii_option);
+}
+
+static struct block *construct_ascii_option(enum opt_name option, const char *string)
+{
+	struct block *new_block;
+	int length;
+	int rounded_length;
+
+	new_block = malloc(sizeof *new_block);
+	length = strlen(string);
+	rounded_length = round_to_dword(length);
+	new_block->data = malloc(rounded_length + 4);
+	new_block->size = rounded_length + 4;
+	*(unsigned short *) new_block->data = option;
+	*(unsigned short *) (new_block->data + 2) = length;
+	memcpy(new_block->data + 4, string, length);
+
+	return new_block;
+}
+
+
+static void generate_section_header(void)
+{
+	struct block *hardware;
+	struct block *os;
+	struct block *userappl;
+	struct block end_of_opt;
+	struct block prefix;
+	char prefix_data[16];
+	char hostname[HOST_NAME_MAX];
+	char version[2048];
+	int fd;
+	uint32_t zero = 0;
+	int result;
+	
+	end_of_opt.data = &zero;
+	end_of_opt.size = sizeof(zero);
+	
+	*(uint32_t *) prefix_data = 0x1a2b3c4d;
+	*(uint16_t *) (prefix_data + 4) = 1;
+	*(uint16_t *) (prefix_data + 6) = 0;
+	*(int64_t *) (prefix_data + 8) = -1L;
+	prefix.data = prefix_data;
+	prefix.size = sizeof(prefix_data);
+
+	gethostname(hostname, sizeof hostname);
+	hardware = construct_ascii_option(shb_hardware,  hostname);
+	
+	fd = open("/proc/version", O_RDONLY);
+	if(fd < 0) {
+		fprintf(stderr, "Cannot open /proc/version: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	result = read(fd, version, sizeof version);
+	assert(result < sizeof version);
+	close(fd);
+	*(version + result) = '\0';
+	
+	os =  construct_ascii_option(shb_os, version);
+	userappl = construct_ascii_option(shb_userappl, "chox capture");
+	
+	write_block(section_header_block,  &prefix, hardware, os, hardware, userappl, &end_of_opt, NULL);
+
+}
+
+static void generate_interface_description(const char *interface, const char *filter)
+{
+	struct block *name_option;
+	struct block *filter_option;
+ 	short prefix_data[4] = { 1, 0, 0, 0};
+	struct block prefix;
+
+	prefix.data = prefix_data;
+	prefix.size = sizeof(prefix_data);
+
+	name_option = construct_ascii_option(if_name, interface);
+	filter_option = construct_ascii_option(if_filter, filter);
+
+	write_block(interface_description, &prefix, name_option, filter_option, &end_of_opt, NULL);
+}
+
+static void prime_pcap_file(const char *interface, const char *filter, const char *filename)
+{
+	output_fd = open(filename, O_WRONLY | O_CREAT);
+	if(output_fd < 0) {
+		fprintf(stderr,  "Cannot create %s: %s\n", filename, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	generate_section_header();
+	generate_interface_description(interface, filter);
+}
+		
+	
+
+int main(int argc, char *argv[])
 {
 	char *dev = NULL;
 	char *filter_exp = NULL;
@@ -95,6 +286,7 @@ main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	prime_pcap_file(dev, filter_exp, output);
 	pcap_loop(handle, 0, got_packet, NULL);
 	                             
 	pcap_freecode(&bpf);
