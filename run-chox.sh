0cd6f35e8b2e000daa3c11a385964f870e2ebf4f
Marty Leisner <maleisner@gmail.com>
2019-03-20 18:18:30 -0400

    improved run-chox script

--- run-chox.sh
+++ run-chox.sh
@@ -2,9 +2,14 @@
 
 
 # use filter= to change the default filter 
-filter="${filter:-tcp}"
+filter=${filter:-tcp}
+
 
 lan_name=chox-lan
+wan_name=chox-wan
+lan_mac=e0:91:f5:6a:78:de 
+wan_mac=e0:91:f5:6a:78:df
+
 if [ "$1" == "gdb" ]; 
 then
 	gdb -x .gdbinit --args  ./chox  -s -f "$filter" -l chox-lan:e0:91:f5:6a:78:de -w chox-wan:e0:91:f5:6a:78:df
@@ -13,5 +18,5 @@ then
 	 valgrind --leak-check=full --show-leak-kinds=all -v  --log-file=valgrind.$$.log \
 			./chox  -s    -f "$filter" -l chox-lan:e0:91:f5:6a:78:de -w chox-wan:e0:91:f5:6a:78:df
 else
-	 exec ./chox -m    -b 500  -f "$filter" -l $lan_name:e0:91:f5:6a:78:de -w chox-wan:e0:91:f5:6a:78:df
+	 exec ./chox -m    -b 500  -f "$filter" -l $lan_name:$lan_mac -w $wan_name:$wan_mac
 fi
